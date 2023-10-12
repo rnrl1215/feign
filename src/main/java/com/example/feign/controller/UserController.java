50ab6652cd61458dbf832defedcc33b7e97eac5e
skahn <d1215@naver.com>
2023-10-12 21:54:40 +0900

    feat: first commit

---
+++ src/main/java/com/example/feign/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.example.feign.controller;
+
+import com.example.feign.Dto.UserDto;
+import com.example.feign.feign.FeignUserClient;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
+import org.apache.catalina.util.ToStringUtil;
+import org.apache.coyote.Request;
+import org.apache.coyote.http11.upgrade.UpgradeServletOutputStream;
+import org.apache.tomcat.util.descriptor.InputSourceUtil;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/v1/users")
+public class UserController {
+
+    private final FeignUserClient feignUserClient;
+
+    @PostMapping("/{id}")
+    public ResponseEntity<UserDto> findUserById(@RequestHeader HttpHeaders httpHeaders,
+                                                @PathVariable(name = "id") String userId,
+                                                @RequestBody(required = false) String body) {
+        UserDto dto = new UserDto("test", "name");
+        return new ResponseEntity<UserDto>(dto, HttpStatus.OK);
+    }
+
+    @PostMapping("/test")
+    public void test() {
+        ResponseEntity<UserDto> test = feignUserClient.findUserId("123", "test");
+        return;
+    }
+}
+
